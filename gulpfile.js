b26ef37895be6234d30c94e42828c4e85b3ea5de
lapp14 <d.lapp14@gmail.com>
2016-05-08 09:42:34 -0400

    Finished gulp build sequence for now

--- gulpfile.js
+++ gulpfile.js
@@ -1,11 +1,12 @@
-var connect = require('gulp-connect');
-var gulp 	= require('gulp');
-var del 	= require('del');
-var less 	= require('gulp-less');
-var useref	= require('gulp-useref');
-var uglify	= require('gulp-uglify');
-var gulpIf  = require('gulp-if');
-var cssnano = require('gulp-cssnano');
+var connect		= require('gulp-connect');
+var gulp 		= require('gulp');
+var del 		= require('del');
+var less 		= require('gulp-less');
+var useref		= require('gulp-useref');
+var ngAnnotate	= require('gulp-ng-annotate');
+var uglify		= require('gulp-uglify');
+var gulpIf  	= require('gulp-if');
+var cssnano 	= require('gulp-cssnano');
 //var browserSync = require('browser-sync').create();
 
 gulp.task('watch', function() {
@@ -29,22 +30,38 @@ gulp.task('less', function() {
 		.pipe(gulp.dest('assets/css'));	
 });
 
+
+gulp.task('build', ['clean:dist', 'build-js', 'build-css', 'build-html', 'build-copy', 'htaccess']);
+
+gulp.task('build-js', function() {
+	return gulp.src(['src/**/*.js'], {base: 'src'} )
+		.pipe(ngAnnotate())
+		.pipe(uglify())
+		.pipe(gulp.dest('dist'));
+});
+
+gulp.task('build-css', function() {
+	return gulp.src(['src/**/*.css'], {base: 'src'} )
+		.pipe(cssnano())
+		.pipe(gulp.dest('dist'));
+});
+
+gulp.task('build-html', function() {
+	return gulp.src(['src/**/*.html'], {base: 'src'} )
+		.pipe(useref())
+		.pipe(gulp.dest('dist'));
+});
+
+gulp.task('build-copy', function() {
+	return gulp.src(['src/**/*.{svg,png,jpg,gif,json}'], {base: 'src'} )
+		.pipe(gulp.dest('dist'));
+});
+
 gulp.task('htaccess', function() {
 	return gulp.src('src/.htaccess')
 		.pipe(gulp.dest('dist'));
 });
 
-gulp.task('build', ['clean:dist', 'htaccess'], function() {
-	return gulp.src(['src/**/*.{html,svg,png,jpg,gif,css,htaccess}'], {
-            base: 'src'
-        }).pipe(useref())
-		//.pipe(gulpIf('*.js', uglify()))
-		.pipe(gulpIf('*.css', cssnano()))
-		.pipe(gulp.dest('dist'));
-});
-
 gulp.task('clean:dist', function() {
-	var d = del.sync('dist');
-	console.log('clean:dist: clean complete');
-	return d;
+	return del.sync('dist');
 });
